0808b97c130d194764c6958f53596925122e83f6
Mc <134487359+Loffholzer@users.noreply.github.com>
2023-11-21 19:16:02 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,10 +20,13 @@ sed -i '/^#Color/s/#//' /etc/pacman.conf
 sed -i '/Color/s//&\nILoveCandy/' /etc/pacman.conf
 
 ##---------------------------------------------------------------------------------------
-## Edit 
+## Edit /etc/pacman.d/mirrorlist
 ##---------------------------------------------------------------------------------------
+cp -f /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.org
 pacman -Sy
-
-
-
+pacman -S --noconfirm --needed reflector
 reflector -a 24 -f 10 -c de -p https --sort rate --save /etc/pacman.d/mirrorlist --verbose
+cp -f /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.tmp
+echo -e "### --- modified by Loffholzer install.sh\n### --- Date: $(date "+%Y-%m-%d | %R")\n### --- (Original-Backup: /etc/pacman.d/mirrorlist.org)\n###\n" >/etc/pacman.d/mirrorlist
+cat /etc/pacman.d/mirrorlist.tmp >>/etc/pacman.d/mirrorlist.org
+rm -rf /etc/pacman.d/mirrorlist.tmp
