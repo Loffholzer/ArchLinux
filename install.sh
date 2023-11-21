889dc3781850b26f8bbdf214ae3f1039062dad40
Mc <134487359+Loffholzer@users.noreply.github.com>
2023-11-21 19:05:55 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,29 @@
 #!/bin/bash
+##-----------------------------------------------------------------------------------
+## author:       Loffholzer
+## project:      https://github.com/Loffholzer/ArchLinux/install.sh
+## version:      0.0.1
+## licence:      MIT
+## date:         2023-11-23
+##-----------------------------------------------------------------------------------
 
+
+##---------------------------------------------------------------------------------------
+## Edit /etc/pacman.conf
+##---------------------------------------------------------------------------------------
+cp -f /etc/pacman.conf /etc/pacman.conf.org
+echo -e "### --- modified by Loffholzer install.sh\n### --- Date: $(date "+%Y-%m-%d | %R")\n### --- (Original-Backup: /etc/pacman.conf.org)\n###\n" >/etc/pacman.conf
+cat /etc/pacman.conf.org >>/etc/pacman.conf
+sed -i 's/^#ParallelDownloads.*/ParallelDownloads = 10/' /etc/pacman.conf
+sed -i '/^#VerbosePkgLists/s/#//' /etc/pacman.conf
+sed -i '/^#Color/s/#//' /etc/pacman.conf
+sed -i '/Color/s//&\nILoveCandy/' /etc/pacman.conf
+
+##---------------------------------------------------------------------------------------
+## Edit 
+##---------------------------------------------------------------------------------------
+pacman -Sy
+
+
+
+reflector -a 24 -f 10 -c de -p https --sort rate --save /etc/pacman.d/mirrorlist --verbose
